f7eae470933608fbe9abff4847c2b27e833b1394
jose-bobba <jbobba@learninghouse.com>
2017-07-19 16:24:17 -0300

    Main routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/newgame/{rows}/{columns}/{mines}', 'GameController@newGame');
+Route::post('/save', 'GameController@save');
+Route::get('/resume/{id}', 'GameController@resume');
+Route::get('/checkCell/{row}/{column}', 'GridController@checkCell');
+Route::post('/mark/{row}/{column}', 'GridController@markCell');
+Route::get('/user/add', 'UserController@add');
+Route::get('/users', 'UserController@getAll');
