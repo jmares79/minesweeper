866956eed0c23fd4a4ce230d62b18edf2d00c30d
jose-bobba <jbobba@learninghouse.com>
2017-07-25 00:17:11 -0300

    Bug fixed. First working version. Tested in small 2x2 matrix

--- app/Service/Grid/GridService.php
+++ app/Service/Grid/GridService.php
@@ -10,7 +10,7 @@ class GridService implements GridCalculatorInterface
 {
     protected $grid;
     protected $cellToCheck = array();
-    protected $visitedCells = array();
+    protected $returnedList = array();
     protected $revealedCells = array();
 
     public function checkCell($grid, $row, $column)
@@ -22,47 +22,19 @@ class GridService implements GridCalculatorInterface
             return $this->revealedCells[] = array(
                 'row' => $row,
                 'column' => $column,
-                'mine' => true,
-                'adjacentMines' => 0
+                'mine' => true
             );
         } else {
             $this->revealAdjacentCells($row, $column);
         }
 
-        return $this->revealedCells;
-
-        // if ($hasMine && !$this->firstCheck($row, $column)) {
-        //     $cell = array_pop($this->cellStack);
-        //     $cell['adjacentMines']++;
-        //     $this->cellStack[] = $cell;
-
-        //     return;
-        // } else {
-        //     $this->cellStack[] = array(
-        //         'row' => $row,
-        //         'column' => $column,
-        //         'mine' => false,
-        //         'adjacentMines' => 0
-        //     );
-
-        //     $this->revealAdjacentCells($row, $column);
-
-        //     $cell = array_pop($this->cellStack);
-        //     $this->revealedCells[] = $cell;
-        // }
-
-        // if (empty($this->cellStack)) {
-        //     return $this->revealedCells;
-        // } else {
-        //     $this->revealAdjacentCells($cell['row'], $cell['column']);
-        // }
+        return $this->returnedList;
     }
 
 
     public function revealAdjacentCells($row, $column)
     {
         echo "HERE with row: $row - col: $column<br>";
-        $this->visitedCells[] = array('row' => $row, 'column' => $column);
 
         if ($this->coordinatesOutOfBounds($row, $column)) { echo "COOB<br>"; return; }
         if ($this->cellAlreadyVisited($row, $column)) { echo "CAV<br>"; return; }
@@ -85,7 +57,6 @@ class GridService implements GridCalculatorInterface
             $this->revealedCells[] = array(
                 'row' => $row,
                 'column' => $column,
-                'mine' => false,
                 'adjacentMines' => 0
             );
 
@@ -100,7 +71,9 @@ class GridService implements GridCalculatorInterface
         $this->revealAdjacentCells($row + 1, $column); //South
         $this->revealAdjacentCells($row + 1, $column - 1); //SW
         $this->revealAdjacentCells($row, $column - 1); //West
-        // dd($this->revealedCells);
+
+        $final = array_pop($this->revealedCells);
+        if (!$this->alreadyLoaded($final)) { $this->returnedList[] = $final; }
     }
 
     protected function hasMine($row, $col)
@@ -115,87 +88,14 @@ class GridService implements GridCalculatorInterface
         return empty($this->revealedCells);
     }
 
-    // protected function revealNWCell()
-    // {
-    //     if ($this->cellToCheck['row'] == 1 || $this->cellToCheck['column'] == 1) { return; }
-    //     echo "<pre>"; var_dump($this->cellToCheck); echo "</pre>";
-    //     echo "Checking NW Cell\n";
+    protected function alreadyLoaded($cell)
+    {
+        foreach ($this->returnedList as $c) {
+            if ($c['row'] == $cell['row'] && $c['column'] == $cell['column']) { return true;}
+        }
 
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] - 1, $this->cellToCheck['column'] - 1);
-    // }
-
-    // protected function revealNorthCell()
-    // {
-    //     if ($this->cellToCheck['row'] == 1) { return; }
-    //     echo "Checking North Cell\n";
-    //     echo "<pre>"; var_dump($this->cellToCheck); echo "</pre>";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] - 1, $this->cellToCheck['column']);
-    // }
-
-    // protected function revealNECell()
-    // {
-    //     if ($this->cellToCheck['row'] == 1 || $this->cellToCheck['column'] == $this->grid->columns) { return; }
-    //     echo "Checking NE Cell\n";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] - 1, $this->cellToCheck['column'] + 1);
-    // }
-
-    // protected function revealEastCell()
-    // {
-    //     if ($this->cellToCheck['column'] == $this->grid->columns) { return; }
-    //     echo "Checking East Cell\n";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'], $this->cellToCheck['column'] + 1);
-    // }
-
-    // protected function revealSECell()
-    // {
-    //     if ($this->cellToCheck['row'] == $this->grid->rows || $this->cellToCheck['column'] == $this->grid->columns) { return; }
-    //     echo "Checking SE Cell\n";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] + 1, $this->cellToCheck['column'] + 1);
-    // }
-
-    // protected function revealSouthCell()
-    // {
-    //     if ($this->cellToCheck['row'] == $this->grid->rows) { return; }
-    //     echo "Checking South Cell\n";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] + 1, $this->cellToCheck['column']);
-    // }
-
-    // protected function revealSWCell()
-    // {
-    //     if ($this->cellToCheck['row'] == $this->grid->rows || $this->cellToCheck['column'] == 1) { return; }
-    //     echo "Checking SW Cell\n";
-
-    //     if ($this->cellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'] + 1, $this->cellToCheck['column'] - 1);
-    // }
-
-    // protected function revealWestCell()
-    // {
-    //     if ($this->cellToCheck['column'] == 1) { return; }
-    //     echo "Checking West Cell\n";
-
-    //     if ($this->CellAlreadyVisited($this->cellToCheck)) { return; }
-
-    //     $this->checkCell($this->grid, $this->cellToCheck['row'], $this->cellToCheck['column'] - 1);
-    // }
+        return false;
+    }
 
     protected function coordinatesOutOfBounds($row, $column)
     {
@@ -212,12 +112,6 @@ class GridService implements GridCalculatorInterface
         foreach ($this->revealedCells as $cell) {
             if ($cell['row'] == $row && $cell['column'] == $column) { return true;}
         }
-        // $cell['row'] = $row;
-        // $cell['column'] = $column;
-        // echo "<pre>"; var_dump($cell); echo "</pre>";
-        // echo "<pre>"; var_dump($this->revealedCells); echo "</pre>";
-        // dd($this->revealedCells);
-        // if (in_array($cell, $this->revealedCells)) { return true; }
 
         return false;
     }
